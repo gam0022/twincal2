7fcecbd02f216fbb8a760cbc7274d477fdc05e43
gam0022 <gam0022@gmail.com>
2014-04-26 15:04:28 +0900

    fix typo

--- tool/database_import.rb
+++ tool/database_import.rb
@@ -9,22 +9,22 @@ require 'csv'
 
 database_filename = "kamoku.db"
 csv_filename = ARGV[0]
-tabale_name = "kamoku"
+table_name = "kamoku"
 
 db = SQLite3::Database.new(database_filename)
 db.busy_timeout(100000)
 
 # kcode	科目名	単位数	標準履修年次	実施学期	曜時限	教室	担当教員	授業概要	備考	科目等履修生履修可否	理由
-db.execute("create table #{tabale_name} (kcode text, 科目名 text, 単位数 text, 標準履修年次 text, 実施学期 text, 曜時限 text, 教室 text, 担当教員 text, 授業概要 text, 備考 text, 科目等履修生履修可否 text, 理由 text)")
+db.execute("create table #{table_name} (kcode text, 科目名 text, 単位数 text, 標準履修年次 text, 実施学期 text, 曜時限 text, 教室 text, 担当教員 text, 授業概要 text, 備考 text, 科目等履修生履修可否 text, 理由 text)")
 
-db.execute("create index kcode_index on #{tabale_name}(kcode)")
+db.execute("create index kcode_index on #{table_name}(kcode)")
 
 table = CSV.parse(File.open(csv_filename).read.encode("UTF-8", "CP932"))
 
 row_count = 0
 
 table.each do |row|
-  str = "insert into #{tabale_name} values(#{row.take(12).map{|s| 
+  str = "insert into #{table_name} values(#{row.take(12).map{|s| 
     s ? "'#{s.gsub("'", "''")}'" : "''"
   }.join(",")})"
   #p str
